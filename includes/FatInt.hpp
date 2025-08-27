7924e8818e28dfd7eca5b63c55ced403533dadff
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2025-08-27 20:53:14 +0200

    wow

--- includes/FatInt.hpp
+++ includes/FatInt.hpp
@@ -1,8 +1,38 @@
 #ifndef FATINT_HPP
 # define FATINT_HPP
 
+#include <cstdint>
+#include <string>
+
 class	FatInt
 {
+	private:
+		union
+		{
+			struct
+			{
+				uint64_t	*start;
+				uint64_t	*end;
+				uint64_t	*memend;
+			};
+			struct
+			{
+				uint64_t	pad[2];
+				uint64_t	num;
+			}
+		};
+
+	public:
+		FatInt();
+		FatInt(uint64_t n);
+		FatInt(int64_t n);
+		FatInt(const std::string &s);//move copy etc
+
+		//operator=(const FatInt &n);
+		FatInt	operator+(const FatInt &n);
+		FatInt	operator-(const FatInt &n);//*/% + bool
+		
+		~FatInt();
 };
 
 #endif
--- src/FatInt.cpp
+++ src/FatInt.cpp
@@ -1,1 +1,48 @@
 #include "FatInt.hpp"
+
+//X-------------------...
+//^signe
+
+//erase le bit de signe, addition, check le bit de signe
+
+FatInt::FatInt() :
+	start(0ULL),
+	end(0ULL)
+{
+}
+
+FatInt::FatInt(uint64_t n)
+{
+}
+
+FatInt::FatInt(int64_t n) :
+{
+}
+
+FatInt::FatInt(const std::string &s)
+{
+}
+
+FatInt::~FatInt()
+{
+}
+
+//check le signe, si - 
+
+FatInt	FatInt::operator+(const FatInt &n)
+{
+	FatInt		res;
+	uint64_t	*ptr = start;
+	uint64_t	*nptr = n.start;
+
+	res.start = new uint64_t[bigptr.end - bigptr.start]
+	while (ptr != end && nptr != n.end)
+	{
+		++ptr;
+		++nptr
+	}
+}
+
+FatInt	FatInt::operator-(const FatInt &n)
+{
+}
